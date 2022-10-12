0bdc22617971487d23320207fd7a85fd53571778
taiki.kida <taiki_kida@r.recruit.co.jp>
2022-10-13 01:53:12 +0900

    従業員newアクションの作成」

--- src/main/java/actions/UnknownAction.java
+++ src/main/java/actions/UnknownAction.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 
 import javax.servlet.ServletException;
 
+import actions.views.EmployeeView;
+import constants.AttributeConst;
 import constants.ForwardConst;
 
 /**
@@ -22,4 +24,20 @@ public class UnknownAction extends ActionBase {
         forward(ForwardConst.FW_ERR_UNKNOWN);
 
     }
+
+
+/**
+ * 新規登録画面を表示する
+ * @throws ServletException
+ * @throws IOException
+ */
+public void entryNew() throws ServletException, IOException {
+
+    putRequestScope(AttributeConst.TOKEN, getTokenId()); //CSRF対策用トークン
+    putRequestScope(AttributeConst.EMPLOYEE, new EmployeeView()); //空の従業員インスタンス
+
+    //新規登録画面を表示
+    forward(ForwardConst.FW_EMP_NEW);
+    }
+
 }
